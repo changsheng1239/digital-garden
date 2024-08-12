0dd65647c39aaae20605d6a2ad09a5bc1a6cab83
Chang <changsheng@sql.com.my>
2024-08-12 13:08:19 +0800

    Quartz sync: Aug 12, 2024, 1:08 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -28,6 +28,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Search(),
     Component.Darkmode(),
     Component.DesktopOnly(Component.Explorer()),
+    Component.DesktopOnly(Component.RecentNotes({ limit: 5 })),
   ],
   right: [
     Component.Graph(),
@@ -45,6 +46,7 @@ export const defaultListPageLayout: PageLayout = {
     Component.Search(),
     Component.Darkmode(),
     Component.DesktopOnly(Component.Explorer()),
+    Component.DesktopOnly(Component.RecentNotes({ limit: 5 })),
   ],
   right: [],
 }
